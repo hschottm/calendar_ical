adcccf18a3d38d5370b66b78aad14151cf3088ba
Daniel Kiesel <icodr8@gmail.com>
2017-07-01 12:53:23 +0200

    added Bundle Module

---
+++ src/CraffftCalendarICalBundle.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Calendar iCal Bundle.
+ *
+ * (c) Daniel Kiesel <https://github.com/iCodr8>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Craffft\CalendarICalBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the Craffft Translation Fields Bundle.
+ */
+class CraffftCalendarICalBundle extends Bundle
+{
+}
