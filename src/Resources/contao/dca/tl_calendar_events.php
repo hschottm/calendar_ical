7d1fbd9be11b7a0bb7710b09cfaa7f7446b72819
Daniel Kiesel <icodr8@gmail.com>
2017-07-02 15:33:51 +0200

    generate ics file when event is saved

--- src/Resources/contao/dca/tl_calendar_events.php
+++ src/Resources/contao/dca/tl_calendar_events.php
@@ -10,6 +10,8 @@
 /**
  * Table tl_calendar_events
  */
+$GLOBALS['TL_DCA']['tl_calendar_events']['config']['onsubmit_callback'][] = array('tl_calendar_events_ical', 'generateICal');
+
 $GLOBALS['TL_DCA']['tl_calendar_events']['list']['global_operations']['export'] =
     array(
         'label'               => &$GLOBALS['TL_LANG']['MSC']['import_calendar'],
@@ -24,3 +26,20 @@ $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['icssource'] = array
     'eval'                    => array('fieldType'=>'radio', 'files'=>true, 'filesOnly'=>true, 'extensions'=>'ics,csv')
 );
 
+class tl_calendar_events_ical extends Backend
+{
+    public function generateICal(DataContainer $dc)
+    {
+        if (!$dc->id) {
+            return;
+        }
+
+        $calendarEvent = \Contao\CalendarEventsModel::findByPk($dc->id);
+
+        if ($calendarEvent !== null) {
+            $this->import('CalendarExport');
+            $this->CalendarExport->exportCalendar($calendarEvent->pid);
+        };
+
+    }
+}
